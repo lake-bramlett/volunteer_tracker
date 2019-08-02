451dd1733b3186749eb6c487ad4ae52fd1691719
Student <example@example.com>
2019-08-02 10:36:11 -0700

    add logic to pass fifth project spec test

--- lib/project.rb
+++ lib/project.rb
@@ -7,6 +7,17 @@ class Project
     @id = attributes.fetch(:id)
   end
 
+  def self.all
+    returned = DB.exec("SELECT * FROM projects ORDER BY title;")
+    projects = []
+    returned.each() do |project|
+      title = project.fetch("title")
+      id = person.fetch("id").to_i
+      people.push(Person.new({:title => title, :id => id}))
+    end
+    projects
+  end
+
   def save
     result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
     @id = result.first().fetch("id").to_i
